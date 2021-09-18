399addf47b2a93d54b63040aae497cd0371bff24
yuxin5 <yuxin5@genomics.cn>
2021-09-18 10:36:04 +0800

    init project

---
+++ spring_alibaba/nacos_practise/src/main/java/com/example/nacos_practise/NacosPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.nacos_practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosPractiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosPractiseApplication.class, args);
+    }
+
+}
---
+++ spring_alibaba/nacos_practise/src/test/java/com/example/nacos_practise/NacosPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nacos_practise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosPractiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
